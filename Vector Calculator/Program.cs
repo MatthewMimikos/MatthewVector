7c1b587edbcb313fdad0460ab1dfddc9482f9c49
Matthew Mimikos <1717212@mukilteo.wednet.edu>
2024-02-05 10:07:46 -0800

    Vectoring Around

--- Vector Calculator/Program.cs
+++ Vector Calculator/Program.cs
@@ -6,11 +6,15 @@ namespace Vector_Calculator
     {
         static void Main(string[] args)
         {
-            Vector test1 = new Vector(1, 2, 3);
+            Vector test1 = new Vector(3, 4, 5);
             Vector test2 = new Vector(-1, 2, -3);
             
             // This should be 0, 4, 0
             Console.WriteLine(Vector.Add(test1, test2));
+            Console.WriteLine(Vector.Negate(test1));
+            Console.WriteLine(Vector.Subtract(test2, test1));
+            Console.WriteLine(Vector.Scale(test1, 5));
+            Console.WriteLine(Vector.Normalize(test1));
         }
     }
 }
--- Vector Calculator/Vector.cs
+++ Vector Calculator/Vector.cs
@@ -11,6 +11,8 @@ namespace Vector_Calculator
         public float y;
         public float z;
 
+        public static float Rutherford { get; private set; }
+
         public Vector(float x, float y, float z)
         {
             this.x = x;
@@ -25,44 +27,43 @@ namespace Vector_Calculator
 
         public float GetMagnitude()
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return MathF.Sqrt((x * x) + (y * y) + (z * z));
         }
 
         public float GetDirection()
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return (float)Math.Atan(y / x);
+            // In Radians
+        }
+        public float GetDirectionDegrees()
+        {
+            return GetDirection() * (180 / MathF.PI);
+            // In Degrees
         }
 
         public static Vector Add(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return new Vector(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
         }
 
         public static Vector Negate(Vector v)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return new Vector(v.x * -1, v.y * -1, v.z * -1);
         }
 
         public static Vector Subtract(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return new Vector(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
         }
 
-        public static Vector Scale(Vector v)
+        public static Vector Scale(Vector v1, int rutherford)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return new Vector(v1.x * rutherford, v1.y * rutherford, v1.z * rutherford);
         }
 
         public static Vector Normalize(Vector v)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return new Vector(v.x * (1 / v.GetMagnitude()), v.y * (1 / v.GetMagnitude()), v.z * (1 / v.GetMagnitude()));
         }
 
         public static float DotProduct(Vector v1, Vector v2)
